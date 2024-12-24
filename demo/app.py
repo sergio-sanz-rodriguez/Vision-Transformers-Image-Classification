3ca90eedcf7e9b6df736b5ec18b8b5f3fa270594
Sergio <sergio.sanz.rodriguez@gmail.com>
2024-12-24 12:06:54 +0100

    update of the transform-eats app

---
+++ demo/app.py
@@ -0,0 +1,148 @@
+
+# Imports and class names setup
+import os
+import torch
+import json
+import gradio as gr
+from model import create_vitbase_model, create_effnetb0
+from timeit import default_timer as timer
+from typing import Tuple, Dict
+from torchvision.transforms import v2
+
+
+# Specify class names
+food_vision_class_names_path = "class_names.txt"
+with open(food_vision_class_names_path, "r") as f:
+    class_names = f.read().splitlines()
+
+# Specify number of classes
+num_classes = len(class_names) - 1 # 101, "unknown" to be discarded
+
+# Load the food description file
+food_descriptions_json = "food_descriptions.json"
+with open(food_descriptions_json, 'r') as f:
+    food_descriptions = json.load(f)
+
+# Instantiate the model
+classification_model_name_path = "effnetb0_classif_epoch13.pth"
+effnetb0_model = create_effnetb0(
+    model_weights_dir=".",
+    model_weights_name=classification_model_name_path,
+    num_classes=2
+    )
+
+# Load the ViT-Base transformer
+food_vision_model_name_path = "vitbase16_5.pth"
+IMG_SIZE = 224
+vitbase_model = create_vitbase_model(
+    model_weights_dir=".",
+    model_weights_name=food_vision_model_name_path,
+    img_size=IMG_SIZE,
+    num_classes=num_classes
+)
+
+# Specify manual transforms
+transforms = v2.Compose([    
+    v2.Resize((242, 242)),
+    v2.CenterCrop((IMG_SIZE, IMG_SIZE)),    
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Normalize(mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]) 
+])
+
+# Predict function
+def predict(img) -> Tuple[Dict, str, str]:
+    """Transforms and performs a prediction on img and returns prediction and time taken.
+    """
+    # Start the timer
+    start_time = timer()
+    
+    # Transform the target image and add a batch dimension
+    img = transforms(img).unsqueeze(0)
+    
+    # Put model into evaluation mode and turn on inference mode
+    effnetb0_model.eval()
+    vitbase_model.eval()
+    with torch.inference_mode():
+
+        # If the picture is food
+        if effnetb0_model(img)[:,1].cpu() >= 0.9981166124343872:
+
+            # Pass the transformed image through the model and turn the prediction logits into prediction probabilities
+            pred_probs = torch.softmax(vitbase_model(img), dim=1) # 101 classes
+
+            # Calculate entropy
+            entropy = -torch.sum(pred_probs * torch.log(pred_probs), dim=1).item()
+
+            # Create a prediction label and prediction probability dictionary for each prediction class
+            pred_classes_and_probs = {class_names[i]: float(pred_probs[0][i]) for i in range(num_classes)}
+            pred_classes_and_probs["unknown"] = 0.0
+
+            # Get the top predicted class
+            top_class = max(pred_classes_and_probs, key=pred_classes_and_probs.get)
+
+            # If the image is likely to be an unknown category
+            if pred_probs[0][class_names.index(top_class)] <= 0.5 and entropy > 2.5:
+
+                # Create prediction label and prediction probability for class unknown and rescale the rest of predictions
+                pred_classes_and_probs["unknown"] = 1.0
+                prob_sum = sum(pred_classes_and_probs.values())
+                pred_classes_and_probs = {key: value / prob_sum for key, value in pred_classes_and_probs.items()}
+
+                # Get the top predicted class
+                top_class = "unknown"
+
+        # Otherwise
+        else:
+
+            # Set all probabilites to zero except class unknown
+            pred_classes_and_probs = {class_names[i]: 0.0 for i in range(num_classes)}
+            pred_classes_and_probs["unknown"] = 1.0
+        
+            # Get the top predicted class
+            top_class = "unknown"
+    
+    # Get the description of the top predicted class
+    top_class_description = food_descriptions.get(top_class, "Description not available.")
+
+    # Calculate the prediction time
+    pred_time = f"{round(timer() - start_time, 1)} s."
+    
+    # Return the prediction dictionary and prediction time 
+    return pred_classes_and_probs, pred_time, top_class_description
+
+# Configure and design the Gradio App
+# Create title, description, and examples
+title = "Transform-Eats Large 🥪🥗🥩"
+description = f"""
+A cutting-edge Vision Transformer (ViT) model to classify 101 delicious food types. Discover the power of AI in culinary recognition.
+
+### Supported Food Types
+{', '.join(class_names[:-1])}.
+"""
+
+food_vision_examples = [["examples/" + example] for example in os.listdir("examples")]
+
+article = "Created by Sergio Sanz."
+
+upload_input = gr.Image(type="pil", label="Upload Image", show_label=True, mirror_webcam=False)
+
+# Create sliders for the thresholds
+#prob = gr.Slider(minimum=0, maximum=1, step=0.05, value=0.4, label="Probability Threshold")
+#entropy = gr.Slider(minimum=0, maximum=4.615, step=0.5, value=2.5, label="Entropy Threshold")
+
+# Create the Gradio demo
+demo = gr.Interface(fn=predict,                                                # mapping function from input to outputs
+                    inputs=upload_input,                                       # inputs
+                    outputs=[gr.Label(num_top_classes=3, label="Prediction"), 
+                             gr.Textbox(label="Prediction time:"),
+                             gr.Textbox(label="Food Description:")],            # outputs
+                    examples=food_vision_examples,                             # Create examples list from "examples/" directory
+                    title=title,                                               # Title of the app
+                    description=description,                                   # Brief description of the app
+                    article=article,                                           # Created by...
+                    theme="ocean")                                             # Theme
+
+# Launch the demo!
+demo.launch()
---
+++ demo/model.py
@@ -0,0 +1,123 @@
+import torch
+import torchvision
+from pathlib import Path
+from vision_transformer import ViT
+
+def load_model(model: torch.nn.Module,
+               model_weights_dir: str,
+               model_weights_name: str):
+
+    """Loads a PyTorch model from a target directory.
+
+    Args:
+    model: A target PyTorch model to load.
+    model_weights_dir: A directory where the model is located.
+    model_weights_name: The name of the model to load.
+      Should include either ".pth" or ".pt" as the file extension.
+
+    Example usage:
+    model = load_model(model=model,
+                       model_weights_dir="models",
+                       model_weights_name="05_going_modular_tingvgg_model.pth")
+
+    Returns:
+    The loaded PyTorch model.
+    """
+    # Create the model directory path
+    model_dir_path = Path(model_weights_dir)
+
+    # Create the model path
+    assert model_weights_name.endswith(".pth") or model_weights_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+    model_path = model_dir_path / model_weights_name
+
+    # Load the model
+    print(f"[INFO] Loading model from: {model_path}")
+    
+    model.load_state_dict(torch.load(model_path, weights_only=True, map_location=torch.device('cpu')))
+    
+    return model
+
+def create_vitbase_model(
+    model_weights_dir:Path,
+    model_weights_name:str,
+    img_size:int=224,
+    num_classes:int=101
+    ):
+    """
+    Creates a ViT-B/16 model with the specified number of classes.
+
+    Args:
+        model_weights_dir: A directory where the model is located.
+        model_weights_name: The name of the model to load.
+        img_size: The size of the input image.
+        num_classes: The number of classes for the classification task.
+
+    Returns:
+    The created ViT-B/16 model.
+    """    
+    # Instantiate the model
+    vitbase16_model = ViT(
+        img_size=img_size,
+        in_channels=3,
+        patch_size=16,
+        num_transformer_layers=12,
+        emb_dim=768,
+        mlp_size=3072,
+        num_heads=12,
+        attn_dropout=0,
+        mlp_dropout=0.1,
+        emb_dropout=0.1,
+        num_classes=num_classes
+    )
+    
+    # Load the trained weights
+    vitbase16_model = load_model(
+        model=vitbase16_model,
+        model_weights_dir=model_weights_dir,
+        model_weights_name=model_weights_name
+        )
+    
+    return vitbase16_model
+
+# Create an EfficientNet-B0 Model
+def create_effnetb0(
+        model_weights_dir: Path,
+        model_weights_name: str,
+        num_classes: int=2,
+        dropout: float=0.2
+        ):
+    """Creates an EfficientNetB0 feature extractor model and transforms.
+
+    Args:
+        model_weights_dir: A directory where the model is located.
+        model_weights_name: The name of the model to load.
+        num_classes (int, optional): number of classes in the classifier head.
+        dropout (float, optional): Dropout rate. Defaults to 0.2.
+
+    Returns:
+        effnetb0_model (torch.nn.Module): EffNetB0 feature extractor model.
+        transforms (torchvision.transforms): Image transforms.
+    """
+    
+    # Load pretrained weights
+    weights = torchvision.models.EfficientNet_B0_Weights.DEFAULT # .DEFAULT = best available weights 
+    effnetb0_model = torchvision.models.efficientnet_b0(weights=weights).to('cpu')
+
+    # Recreate the classifier layer and seed it to the target device
+    effnetb0_model.classifier = torch.nn.Sequential(
+        torch.nn.Dropout(p=dropout, inplace=True), 
+        torch.nn.Linear(in_features=1280, 
+                        out_features=num_classes,
+                        bias=True))
+    
+    # Create the model directory path
+    model_dir_path = Path(model_weights_dir)
+
+    # Create the model path
+    assert model_weights_name.endswith(".pth") or model_weights_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+    model_path = model_dir_path / model_weights_name
+
+    # Load the state dictionary into the model
+    effnetb0_model.load_state_dict(torch.load(model_path, weights_only=True, map_location=torch.device('cpu')))
+        
+    return effnetb0_model
